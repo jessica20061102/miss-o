1630be7679592e89e841f6ecf8b847351883deb5
jessica20061102 <127757916+jessica20061102@users.noreply.github.com>
2024-08-30 07:55:43 -0300

    lucasss

--- script.js
+++ script.js
@@ -8,44 +8,54 @@ const perguntas = [
     {
         enunciado: "Na sua opinião, considerando a cultura contemporânea do 'fazer menos é fazer mais', a prática da castração de animais deve ser vista como: uma medida necessária para controlar a superpopulação e promover a saúde dos animais; ou como uma intervenção desnecessária que pode interferir nos processos naturais de reprodução?",
         alternativas: [
-            "A prática da castração de animais deve ser vista como uma medida necessária para controlar a superpopulação e promover a saúde dos animais, alinhando-se com a ideia de que menos animais significa mais qualidade de vida para cada um deles.",
-            "A castração de animais pode ser vista como uma intervenção desnecessária que interfere nos processos naturais de reprodução, desconsiderando que os ecossistemas têm capacidade de ajustar suas populações naturalmente."
+            {
+                texto:"Reduzindo os custos de produção, tornando os alimentos mais acessíveis.",
+                afirmacao:"Reduzir os custos da produção é uma estratégia eficaz para tornar os alimentos mais acessíveis, promovendo uma alimentação saudavel e economica para todos."
+
+            },
+            {
+           texto:"A prática da castração de animais deve ser vista como uma medida necessária para controlar a superpopulação e promover a saúde dos animais, alinhando-se com a ideia de que menos animais significa mais qualidade de vida para cada um deles.",
+           afirmacao:"A castração de animais pode ser vista como uma intervenção desnecessária que interfere nos processos naturais de reprodução, desconsiderando que os ecossistemas têm capacidade de ajustar suas populações naturalmente."
+            }
         ]
     },
-    {
-        enunciado: "Qual é a visão de cada tribo indigena  diante da castração de animais sendo que é  realidade  a cultura do menos é mais já é pertinente?",
-        alternativas: [
-            "Desafiador.",
-            "Apoiam as medidas já tomadas."
-        ]
-    },
-    {
-        enunciado: "Como utlizar a tecnologia para promover ações objetivas para aumentar o número de castrações, e assim diminuir a quantidade de animais em situação de rua, e ações de conscientização, promovendo a mudança cultural para evitar o abandono de animais, utilizando hábitos do menos é mais?",
-        alternativas: [
-            "Uso das redes sociais para apoio e divulgação das ações.",
-            "Ser parceiro das ações."
-        ]
-    }
 ];
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
+    textoResultado.textContent = "";
     mostraAlternativas();
 }
 
 function mostraAlternativas(){
-    for (const alternativa of perguntaAtual.alternativas){
+    for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas);
-
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener('click', ()=> respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);        
     }
-
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal = afirmacao;
+    atual++
+    mostraPergunta();
 }
 
 mostraPergunta();
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2030...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
